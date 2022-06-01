8c814b9cc3fa719ba20856ffce53388b617f3da5
Rachmat Purwa Saputra <rachmatpurwa393@gmail.com>
2022-06-01 10:35:54 +0700

    Modify binaryTree and test_Calc

--- test_Calc.c
+++ test_Calc.c
@@ -1,3 +1,11 @@
+/**
+ *  Program file name   : testCalc.c
+ *  Description         : Main driver untuk program Kakuraato
+ *  Author              : Danu Mahesa, 211524037 || Extended by : Rachmat Purwa Saputra, 211524054
+ *  Compiler            : GCC
+ *
+ */
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -9,44 +17,66 @@
 #include "tampilan.c"
 
 int main() {
+	// Inisialisasi jendela cmd agar bersih dan berukuran 145 x 40
     system("cls");
-    system("mode 145, 40");
+    // Make the console windows become full-size
+	ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
 	
     char infix[21];
     char postfix[21];
-    char treeHistory[41] = "";
+
 	char inputUser;
 	char inputUserStd = 48;
+
+	// Lakukan selama user belum memilih untuk keluar dari aplikasi
 	do{	
 		system("cls");
 		tampilMainMenu();
 	    printf("\n");
+
 		gotoxy(64,23);
-	
 		scanf(" %c",&inputUser);	
-		if(inputUser == 49){
+
+		if (inputUser == 48) {
+			break;
+		}
+		else if (inputUser == 49){
 			do{	
 				tampilKalkStandar();
+
 				gotoxy(57,5);
-				scanf(" %[^\n]%*c2", infix);
+				scanf(" %[^\n]%*c", infix);
+
 				infix_to_postfix(infix, postfix);
-				addr treeRoot = BuildTreeFromPostfix(postfix);
+
+				addr treeRoot = createTreeFromPostfix(postfix);
+
+				// gotoxy(57, 15);
+				// printTree(treeRoot);
+
 				gotoxy(57,9);
-				printf(" %.2f", TreeCalculate(treeRoot));
+				printf(" %.2f", calculateTreeExpression(treeRoot));
 				getche();
-				gotoxy(88,13);
+
+				gotoxy(91,13);
 				scanf(" %c", &inputUserStd);
-			} while(inputUserStd != 81);
-		}else if(inputUser == 50){
-			tampilKalkAkur();
-		}else if(inputUser == 51){
-			tampilHelp();
-		}else if(inputUser == 52){
-			tampilHistory();	
-		}else{
-			exit(0);
+			} while (inputUserStd != 81);  // selama user tidak menekan karakter 'Q' dari keyboard
 		}
-	}while(inputUser != 48);
+		else if (inputUser == 50) {
+			tampilKalkAkur();
+		}
+		else if (inputUser == 51) {
+			tampilHelp();
+		}
+		else if (inputUser == 52) {
+			tampilHistory();	
+		}
+		else {
+			printf("\nHarap masukkan angka 0, 1, 2, 3, atau 4 saja!\n");
+			getche();
+		}
+	} while (inputUser != 48);  // selama bukan digit 0 yang dipilih user
+
     return 0;
 }
 
