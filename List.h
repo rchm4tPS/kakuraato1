d7f02c2f86e26a61806d26a1ab3c9df95cf39888
Rachmat Purwa Saputra <rachmatpurwa393@gmail.com>
2022-06-01 12:32:47 +0700

    Modify list so it can contain time array and result of calculation

--- List.h
+++ List.h
@@ -1,3 +1,11 @@
+/**
+ *  Program file name   : List.h
+ *  Description         : deklarasi header file untuk struktur data linked list dinamis pada program Kakuraato.
+ *  Author              : Husna Maulana, 211524045
+ *  Compiler            : GCC
+ *
+ */
+
 #ifndef LIST_H
 #define LIST_H
 
@@ -8,50 +16,48 @@
 #include <string.h>
 
 #define Nil NULL
-#define Info(P) (P)->info
-#define Next(P) (P)->next
 
 typedef struct tNodeList* address;
 typedef struct tNodeList {
 	char expression[40];
+	double result;
+	char timeEnteringExpression[64];
 	address next;
 }NodeList;
 typedef struct{
 	address root;
 } List;
 	  
-void CreateList(List *L);
 /* 	I.S : L terdefinisi, tidak diketahui nilainya
-	F.S : L diinisialisasi first(L) = nil
+	F.S : L diinisialisasi L.root = nil
 */
+void CreateList(List *L);
 
-address Alokasi(char* expression);
 /*	Mengirimkan address hasil alokasi sebuah elemen
 	Jika alokasi berhasil, maka address !=Nil,
-	dan misalnya menghasilkan P, maka Info(P)= x, Next(P)=Nil
 	Jika alokasi gagal, mengirimkan Nil
 */
+address Alokasi(char* expression, double result, char* timeOfCalculation);
 
-void DeAlokasi (address P);
 /*	I.S : P terdefinisi
 	F.S : P dikembalikan ke sistem
-	Melakukan dealokasi/ pengembalian address P ke system.
+			Melakukan dealokasi/ pengembalian address P ke system.
 */
+void DeAlokasi (address P);
 
-
-void InsLast (List *L, char* expression);
 /*I.S : L terdefinisi, mungkin kosong
-F.S : melakukan alokasi sebuah elemen dan
-menambahkan elemen terakhir pada List dengan nilai X jika alokasi
-berhasil. Jika alokasi gagal IS = FS
+  F.S : melakukan alokasi sebuah elemen dan
+		menambahkan elemen terakhir pada List dengan array expression jika alokasi
+		berhasil. Jika alokasi gagal IS = FS
 */
+void InsLast (List *L, char* expression, double result, char* timeOfCalculation);
 
-void PrintInfo(List L);
 /* I.S : L terdefinisi, mungkin kosong
-F.S : Jika List tidak kosong, menampilkan semua info yang disimpan pada
-elemen list ke layar. jika List kosong, hanya menampilkan pesan “list
-kosong”
-*/
+   F.S : Jika List tidak kosong, menampilkan semua info yang disimpan pada
+		 elemen list ke layar.
+*/  
+void PrintInfo(List L);
+
 
 #endif
 
